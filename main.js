d4f0563243ef92b266a14b08dfde2c8ece6f4ce2
tastytim <tastytimgm@gmail.com>
2021-10-11 14:39:45 +0200

    commit 2 option 2

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-document.getElementsByClassName("fa-bars")[0].addEventListener("click", function(){
-    document.getElementsByClassName("hamburger-menu")[0].classList.add("close");
-}); 
+// document.getElementsByClassName("fa-bars")[0].addEventListener("click", function(){
+//     document.getElementsByClassName("hamburger-menu")[0].classList.add("close");
+// }); 
 
 
-document.getElementsByClassName("fa-times")[0].addEventListener("click", function(){
-    document.getElementsByClassName("hamburger-menu")[0].classList.remove("close");
-}); 
+// document.getElementsByClassName("fa-times")[0].addEventListener("click", function(){
+//     document.getElementsByClassName("hamburger-menu")[0].classList.remove("close");
+// }); 
+
+
+
+const bars = document.querySelector(".fa-bars");
+const times = document.querySelector(".fa-times");
+const hamburger = document.querySelector(".hamburger-menu");
+
+
+bars.addEventListener("click" , function(){
+    hamburger.classList.add("close");
+});
+
+times.addEventListener("click" , function(){
+    hamburger.classList.remove("close");
+});
